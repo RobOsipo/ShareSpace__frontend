54697f60702b00841dde6fd4a0347b5d4952f2c6
Robert <osipovitchr@gmail.com>
2022-05-12 17:16:07 -0400

    more basic setup + styles + dummyInfo

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
+    const TEMPUSERS = [{id: 'u1', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pjxg0Tpg4MuGlrPzQEvRlgHaE8%26pid%3DApi&f=1', name: 'Rob', places: 3 }]
   return (
-    <h2>Users</h2>
+    <UsersList items={TEMPUSERS} />
   )
 }
 
