21f56ea8d10c8fe98eb1eb670f7048ae67fe0185
Robert <osipovitchr@gmail.com>
2022-05-14 18:45:46 -0400

    added google maps API for users to view places

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewPlace from './places/pages/NewPlace'
 import UserPlaces from './places/pages/UserPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 const App = () => {
+  
   return (
    <>
     <MainNavigation />
