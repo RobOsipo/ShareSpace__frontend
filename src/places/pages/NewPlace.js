3b68fa7ee277e29e2d5c975070abccbe97f45289
Robert <osipovitchr@gmail.com>
2022-05-21 15:24:08 -0400

    create util validator file + handle basic front-end input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+
+import './NewPlace.css'
+
 const NewPlace = () => {
   return (
-    <h2>Place</h2>
+    <form className="place-form">
+      <Input type="text" label="Title" element="input" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
+    </form>
   )
 }
 
