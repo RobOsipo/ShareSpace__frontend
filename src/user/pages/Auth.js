41a2a24d855673d06fbd41b260d91bc98fad8db4
Robert <osipovitchr@gmail.com>
2022-05-30 13:34:20 -0400

    connect client to server + ability to register users from client

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -29,10 +29,35 @@ const Auth = () => {
     false
   );
 
-  const authSubmitHandler = (e) => {
+  const authSubmitHandler = async (e) => {
     e.preventDefault();
 
-    console.log(formState.inputs);
+    if (isLoginMode) {
+
+    
+     
+    } else {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/register', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        })
+        const data = await response.json()
+        console.log(data)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    
+    
     auth.login()
   };
 
